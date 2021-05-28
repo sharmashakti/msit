f7188e57f33ed05e83322daae67066120cce8490
shakti sharma <shaktisharma@outlook.com>
2021-05-28 15:11:05 +0530

    Added Chapter 1,3 and 4 source code

---
+++ microservice architecture/ch-1-building-your-first-ASP.NET-Core-App/asp.netcore-app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace aspnetcore_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+         public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel();
+                    webBuilder.UseStartup<Startup>();
+                });    
+    }
+}
